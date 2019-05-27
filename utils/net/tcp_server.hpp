3b50b15ca6be9ce9d0023aa42c00feeaba7e0a26
sujiahong <1511877962@qq.com>
2019-05-27 09:51:01 +0800

    epoll

--- utils/net/tcp_server.hpp
+++ utils/net/tcp_server.hpp
@@ -11,14 +11,15 @@
 class TCPServer{
 private:
     int m_listenSocketfd;
+    int m_eopllfd;
     int m_connectionArray[65536]= {0};
     char m_sendBuff[4096];
     char m_recvBuff[4096];
 public:
     TCPServer(int port){
-        int ret;
-        socklen_t len;
+        int ret, nfds, i, connSockfd;
         struct sockaddr_in servaddr, cliaddr;
+        socklen_t len = sizeof(cliaddr);
         m_listenSocketfd = socket(AF_INET, SOCK_STREAM, 0);
         servaddr.sin_family = AF_INET;
         servaddr.sin_addr.s_addr = htonl(INADDR_ANY);
@@ -26,9 +27,30 @@ public:
         bind(m_listenSocketfd, (struct sockaddr*)&servaddr, sizeof(servaddr));
         listen(m_listenSocketfd, 4096);
         printf("server listen success !!\n");
-        int connSockfd;
-        epollfd = epoll_create1(0);
+        m_eopllfd = epoll_create1(0);
         struct epoll_event ev, events[10];
+        ev.events = EPOLLIN;
+        ev.data.fd = m_listenSocketfd;
+        epoll_ctl(m_epollfd, EPOLL_CTL_ADD, m_listenSocketfd, &ev);
+        while(1){
+            nfds = epoll_wait(m_eopllfd, events, 10, -1);
+            if (nfds == -1){
+                
+            }
+            for(i = 0; i < nfds; ++i){
+                if (events[i].data.fd == m_listenSocketfd){
+                    connSockfd = accept(m_listenSocketfd, (struct sockaddr*)&cliaddr, &len);
+                    if (connSockfd < 0){
+
+                    }
+                    ev.events = EPOLLIN | EPOLLET;
+                    ev.data.fd = connSockfd;
+                    epoll_ctl(m_eopllfd, EPOLL_CTL_ADD, connSockfd, &ev);
+                }else{
+
+                }
+            }
+        }
         while(1){
             len = sizeof(cliaddr);
             connSockfd = accept(m_listenSocketfd, (struct sockaddr*)&cliaddr, &len);
